b8074ec984281c7af1213fa9767b125109fa209f
Justin James <justin.james@yale.edu>
2020-05-05 05:57:40 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  new_array = []
+  while array[counter] do 
+    exponent = array[counter]**2 
+    new_array.push(exponent)
+    counter += 1 
+  end
+  new_array
 end
